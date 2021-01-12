7f5761aa8bafad3452c2d3efbda53b23bdf9dec9
rashmi <rashmidash18@gmail.com>
2021-01-12 14:16:21 +0530

    commiting report

---
+++ Sprint2/HTML_report/tag1signin/report.js
@@ -0,0 +1,90 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Features/Signingin.feature");
+formatter.feature({
+  "line": 2,
+  "name": "Sigining in",
+  "description": "",
+  "id": "sigining-in",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@Sigining_in"
+    }
+  ]
+});
+formatter.background({
+  "line": 4,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 5,
+  "name": "user should be on homepage of eBay website",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "user clicks Sign in",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefinition.user_should_be_on_homepage_of_eBay_website()"
+});
+formatter.result({
+  "duration": 21868989800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_clicks_sign_in()"
+});
+formatter.result({
+  "duration": 10941866900,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 9,
+  "name": "successful entering email or username",
+  "description": "",
+  "id": "sigining-in;successful-entering-email-or-username",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 8,
+      "name": "@tag1"
+    }
+  ]
+});
+formatter.step({
+  "line": 10,
+  "name": "enter valid email or username",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 11,
+  "name": "click continue",
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "valid email or username",
+      "offset": 6
+    }
+  ],
+  "location": "StepDefinition.enter(String)"
+});
+formatter.result({
+  "duration": 0,
+  "error_message": "cucumber.runtime.AmbiguousStepDefinitionsException: ✽.When enter valid email or username(Features/Signingin.feature:10) matches more than one step definition:\n  ^enter (.+)$ in StepDefinition.enter(String)\n  ^enter valid email or username$ in StepDefinition.enter_valid_email_or_username()\n\r\n\tat cucumber.runtime.RuntimeGlue.stepDefinitionMatch(RuntimeGlue.java:71)\r\n\tat cucumber.runtime.Runtime.runStep(Runtime.java:267)\r\n\tat cucumber.runtime.model.StepContainer.runStep(StepContainer.java:44)\r\n\tat cucumber.runtime.model.StepContainer.runSteps(StepContainer.java:39)\r\n\tat cucumber.runtime.model.CucumberScenario.run(CucumberScenario.java:44)\r\n\tat cucumber.runtime.junit.ExecutionUnitRunner.run(ExecutionUnitRunner.java:102)\r\n\tat cucumber.runtime.junit.FeatureRunner.runChild(FeatureRunner.java:63)\r\n\tat cucumber.runtime.junit.FeatureRunner.runChild(FeatureRunner.java:18)\r\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:238)\r\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:63)\r\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:236)\r\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:53)\r\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:229)\r\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:309)\r\n\tat cucumber.runtime.junit.FeatureRunner.run(FeatureRunner.java:70)\r\n\tat cucumber.api.junit.Cucumber.runChild(Cucumber.java:95)\r\n\tat cucumber.api.junit.Cucumber.runChild(Cucumber.java:38)\r\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:238)\r\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:63)\r\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:236)\r\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:53)\r\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:229)\r\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:309)\r\n\tat cucumber.api.junit.Cucumber.run(Cucumber.java:100)\r\n\tat org.eclipse.jdt.internal.junit4.runner.JUnit4TestReference.run(JUnit4TestReference.java:89)\r\n\tat org.eclipse.jdt.internal.junit.runner.TestExecution.run(TestExecution.java:41)\r\n\tat org.eclipse.jdt.internal.junit.runner.RemoteTestRunner.runTests(RemoteTestRunner.java:542)\r\n\tat org.eclipse.jdt.internal.junit.runner.RemoteTestRunner.runTests(RemoteTestRunner.java:770)\r\n\tat org.eclipse.jdt.internal.junit.runner.RemoteTestRunner.run(RemoteTestRunner.java:464)\r\n\tat org.eclipse.jdt.internal.junit.runner.RemoteTestRunner.main(RemoteTestRunner.java:210)\r\n",
+  "status": "failed"
+});
+formatter.match({
+  "location": "StepDefinition.click_continue()"
+});
+formatter.result({
+  "status": "skipped"
+});
+});
