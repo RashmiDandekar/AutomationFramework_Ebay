0577800418d169c1563d1514e26aaea6c9919676
haritha <haritha.lokavarapu@gmail.com>
2021-01-12 22:10:59 +0530

    Testrunner conflict solved

--- Sprint2/src/test/java/CucumberTest/TestRunner.java
+++ Sprint2/src/test/java/CucumberTest/TestRunner.java
@@ -10,15 +10,11 @@ import cucumber.api.junit.Cucumber;
 	
 	features = {"Features/Buy.feature","Features/Sell.feature", "Features/ContinueWithGoogle.feature", "Features/Dailydeals.feature", "Features/HelpAndContact.feature"}
 	,glue={"StepDefinition"}
-<<<<<<< HEAD
-	,plugin= {"html:HTML_report/tag1signin","json: JSON_report/tag1signin", "junit: Junit_report/tag1signin"}
-	,tags={"@tag1"}
-	)
-=======
+
 	,plugin= {"html:HTML_report/html_report","json: JSON_report/json_report", "junit: Junit_report/junit_report"}
 	
 			)
->>>>>>> branch 'master' of https://github.com/rashmidan/Sprint2
+
 	public class TestRunner {
 		
 	}
